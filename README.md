7fe30deb405cd28a5eef10a4eb9c3f7cd6cb9228
b-ma <benjamin.matuszewski@gmail.com>
2015-07-09 18:00:04 +0200

    first template prototype

--- README.md
+++ README.md
@@ -7,3 +7,35 @@ The interesting fact here is that Docchi is working in ES6.
 ```
 docchi < ./path/to/es6/es2015/js | docchidown | mustache - node_modules/docchidown/jsdoc.template > output.md
 ```
+
+
+## Example usage
+
+### Command Line
+
+
+
+### API
+
+```
+docchidown.parse({
+  // the output from docchi (mandatory)
+  source: docchiDoc.out({ render: false }),
+  // defaults to 'json'
+  outputType: 'markdown',
+  // the file in which write the result (relative to `process.cwd()`)
+  destination: './path/to/file',
+});
+```
+
+`docchidown.parse` returns a Promise so you can also :
+
+```
+docchidown
+  .parse(options)
+  .then(function(result) {
+    doSomethingWithResult(result);
+  });
+```
+
+
--- docchidown.js
+++ docchidown.js
@@ -3,28 +3,66 @@
  *  @output {json}
  */
 var fs = require('fs');
+var path = require('path');
+var _ = require('underscore');
 
 'use strict';
 
 module.exports.parse = function(options) {
+  // docchi.output();
   var source = options.source;
-  var destination = options.destination;
+  // if defined the path of the file where we should write the output
+  var destination = path.join(process.cwd(), options.destination);
+  // configurable output type (json|markdown)
+  var outputType = options.outputType || 'json';
+  // path to the md template
+  var templatePath = options.template || path.join(__dirname, 'templates', 'wavesdoc.tmpl');
 
   return Promise.resolve(source)
     .then(cleanDocchi)
     .then(getTargets)
     .then(reorder)
     .then(function(result) {
-      if (destination) {
-        // write file sync
-      } else {
-        return result;
+      switch (outputType) {
+        case 'json':
+          if (destination) {
+            var json = JSON.stringify(result, null, 2);
+            fs.writeFileSync(destination, json);
+          }
+
+          return result;
+          break;
+
+        // could use potentially any type of output as long as the given template works
+        default:
+          var templateSrc = fs.readFileSync(templatePath).toString();
+          // var template = Handlebars.compile(templateSrc);
+          // var rendered = template(result);
+          // var rendered = swig.render(templateSrc,  });
+          // var rendered = swig.render(templateSrc, result );
+          var template = _.template(templateSrc);
+          var rendered = template({ results: result });
+          // remove template indentation
+          // rendered = rendered.replace(/^ +/gm, '');
+          rendered = rendered.replace(/\n{2,}/gm, '\n\n');
+          rendered = rendered.replace(/(.*)$/gm, '$1  ');
+
+          // replace ``` with {% highlight js %} ? maybe later...
+
+          if (destination) {
+            fs.writeFileSync(destination, rendered);
+          }
+
+          return rendered;
+          break;
       }
+    }).catch(function(err) {
+      console.error(err.stack);
     });
 }
 
 /**
- *  Clean docchi output
+ *  Flatten and clean docchi output
  */
 function cleanDocchi(json) {
   var result = [];
@@ -52,8 +90,14 @@ function cleanDocchi(json) {
             return tag.title === 'this';
           })[0];
 
-          block.target = targetTag.name;
-          block.tags.splice(block.tags.indexOf(targetTag), 1);
+          // don't crash if @this is not defined
+          if (targetTag) {
+            block.target = targetTag.name;
+            block.tags.splice(block.tags.indexOf(targetTag), 1);
+          } else {
+            // remove this block if no target
+            json.splice(json.indexOf(block), 1);
+          }
         }
       }
 
@@ -75,11 +119,12 @@ function _getJSONPointerFromTarget(json, target) {
   json.forEach(function(block, index) {
     if (block.name === target) { pointer = index; }
   });
+
   return pointer;
 }
 
 /**
- * find target for each block
+ * Find the target for each block
  */
 function getTargets(json) {
   var toReorder = [];
@@ -89,6 +134,8 @@ function getTargets(json) {
     json.forEach(function(block, index) {
       if (block.target) {
         var pointer = _getJSONPointerFromTarget(json, block.target);
+        if (pointer === undefined) { return; }
+
         toReorder.push([index, pointer]);
       }
       // delete block.target;
@@ -110,20 +157,27 @@ function reorder(data) {
       var source = json[instruction[0]];
       var target = json[instruction[1]];
       var type = '_' + source.type;
-      // delete source.type;
+
+      // if (!source || !target) { return; }
 
       target[type] = target[type] || [];
       target[type].push(source);
 
       // remove source from tree
       delete json[instruction[0]];
+      // console.log(json.indexOf(source));
+      // json.splice(json.indexOf(source), 1);
     });
 
+    // clean all dummy entries
     for (var i = json.length; i >= 0; i--) {
       if (json[i] === undefined) {
         json.splice(i, 1);
+      } else if (json[i].type !== 'class') {
+        json.splice(i, 1);
       }
     }
+
     resolve(json);
   })
 }
